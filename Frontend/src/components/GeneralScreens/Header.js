b23f065c1dd23a3b9b113ef287dfc23c3ac8b6da
Ansh Makker <anshmakker2002@gmail.com>
2024-07-06 12:17:33 +0530

    changes are done

--- Frontend/src/components/GeneralScreens/Header.js
+++ Frontend/src/components/GeneralScreens/Header.js
@@ -38,7 +38,7 @@ const Header = () => {
 
                 <Link to="/" className="logo">
                     <h5>
-                        MERN BLOG
+                        Blog Application
 
                     </h5>
                 </Link>
